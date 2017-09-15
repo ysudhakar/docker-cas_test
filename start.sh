cf3e8cfb302ddade8d4ba7999ba952acc274aec2
Jared King <jared.king@biola.edu>
2017-09-15 10:06:47 -0700

    Using stable container names

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ while ! echo exit | nc localhost 11311 >/dev/null 2>&1; do sleep 1; done
 while ! echo exit | nc localhost 28017 >/dev/null 2>&1; do sleep 1; done
 
 # Load sample data into MongoDB
-docker exec -i castest_mongodb_1 mongo < test_users.js
+docker exec -i castest_mongodb mongo < test_users.js
 
 # Start up CAS
 docker-compose up -d app
